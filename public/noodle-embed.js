a7b266d39f50dfbf32cb92800e6cbe28b250aff7
Joseph Rousseau <joerou@gmail.com>
2021-10-05 11:06:32 -0400

    maybe

--- public/noodle-embed.js
+++ public/noodle-embed.js
@@ -1,9 +1,10 @@
 window.addEventListener( 'load', (event) => {
 
   document.head.innerHTML += '<link href="https://panda-rocket-punch.github.io/embed-social-test/docs/index.css" rel="stylesheet" />';
-  document.head.innerHTML += '<script src="https://panda-rocket-punch.github.io/embed-social-test/index.js" />';
+  // document.head.innerHTML += '<script src="https://panda-rocket-punch.github.io/embed-social-test/index.js" />';
 
   const iDiv = document.createElement('div');
   iDiv.id = 'noodle-social-chat';
   document.body.appendChild(iDiv);
+
 });
